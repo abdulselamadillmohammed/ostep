2d9e7b3af36f81665acf323419883d6721dbc637
Ashadowmonarch <forcoding39@gmail.com>
2025-06-03 09:18:20 -0400

    new file

---
+++ adder.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    /* char is 8-bit integer */
+    signed char a = 100;
+    signed char b = 120;
+    signed char s = a + b;
+    printf("%d\n", s);
+    return 0;
+
+}
